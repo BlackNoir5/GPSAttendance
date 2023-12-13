0087033a1950181f112f5bff5c98becbcc14c295
marlin-1013 <kjskjs11288@naver.com>
2023-12-13 16:56:38 +0900

    파일 경로변경

--- src/main/java/CC/com/spring/ex/controller/SuggestController.java
+++ src/main/java/CC/com/spring/ex/controller/SuggestController.java
@@ -38,8 +38,8 @@ public class SuggestController {
             return showMessageAndRedirect("파일이 없습니다.", "/userPage", model);
         }else {
             String fileName = file.getOriginalFilename();
-            String fileFolder = "/home/server1/CAS/build/libs";
-            String fileRoot = "/" + uid + "/" + week + "주차_" + fileName;
+            String fileFolder = "/home/server1/CAS/build/libs/" + uid + "/" + week;
+            String fileRoot = "주차_" + fileName;
 
             System.out.println(uid + week + suggest + fileName + fileRoot);
 
@@ -54,8 +54,7 @@ public class SuggestController {
                     e.getStackTrace();
                 }
             }
-            String fullFile = fileFolder + fileRoot;
-            File path = new File(fullFile);
+            File path = new File(fileFolder, fileRoot);
             try{
                 file.transferTo(path);
                 SuggestEntity result = suggestService.save(entity);
